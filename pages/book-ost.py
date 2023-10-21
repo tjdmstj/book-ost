631baa47129dc8e71e5d59c151b8541adc3aabf0
Eun-Seo Seo <101198913+tjdmstj@users.noreply.github.com>
2023-10-22 04:10:34 +0900

    Update book-ost.py

--- pages/book-ost.py
+++ pages/book-ost.py
@@ -203,10 +203,8 @@ target_url = 책정보['url']
 options = Options()
 # 창 숨기는 옵션 추가
 options.add_argument('headless')
-release = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
-version = requests.get(release).text
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=Service(executable_path='/Users/seoeunseo/Desktop/deep.daiv/프로젝트/project_run/chromedriver.exe'), options=options)
 driver.get(target_url)
 
 img= driver.find_element(By.XPATH, '//*[@id="tabContent"]/div[1]/div[1]/div[1]/span/img')
